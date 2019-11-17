ea9fe060b8f4863ab5473d7219b579109566f980
SuyamaDaichi <symdit@gmail.com>
2019-11-17 15:37:29 +0900

    SMSをLINEに通知

--- NotifySMSToLine/Function.cs
+++ NotifySMSToLine/Function.cs
@@ -10,25 +10,31 @@ using Amazon.Lambda.Core;
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 
-namespace NotifyIncomingToAnyService
+namespace NotifySMSToLine
 {
     public class Function
     {
         private static HttpClient client = new HttpClient();
 
         /// <summary>
-        /// 着信したらLINE Notifyに通知する
+        /// SMSが来たらLINE Notifyに通知する
         /// </summary>
         /// </summary>
         /// <param name="input"></param>
         /// <param name="context"></param>
         /// <returns></returns>
-        public async Task<object> FunctionHandler(TelInfo input, ILambdaContext context)
+        public async Task<object> FunctionHandler(SMSInfo input, ILambdaContext context)
         {
             var json = @"{""foo"":""hoge"", ""bar"":123, ""baz"":[""あ"", ""い"", ""う""]}";
             var content = new StringContent(json, Encoding.UTF8);
+
+            // リージョンを日本に設定
+            var culture = System.Globalization.CultureInfo.GetCultureInfo("ja-JP");
+
+            string sentDate = DateTimeOffset.FromUnixTimeSeconds(input.sentTimestamp).AddHours(9).ToString("yyyy/MM/dd(ddd) HH:mm:ss", culture);
+
             client.DefaultRequestHeaders.Add("Authorization", $"Bearer {Environment.GetEnvironmentVariable("accessToken")}");
-            var result = await client.PostAsync($"https://notify-api.line.me/api/notify/?message={input.telNum ?? "非通知"}から着信がありました", content);
+            var result = await client.PostAsync($"https://notify-api.line.me/api/notify/?message=\n【内容】\n{input.messageContent}\n\n【受信時刻】\n{sentDate}\n\n【送信元】\n{input.senderPhoneNumber}", content);
             client = new HttpClient();
             return result;
         }
---
+++ NotifySMSToLine/SMSInfo.cs
@@ -0,0 +1,18 @@
+﻿namespace NotifySMSToLine
+{
+    public class SMSInfo
+    {
+        /// <summary>
+        /// 送信元電話番号
+        /// </summary>
+        public string senderPhoneNumber { get; set; }
+        /// <summary>
+        /// SMSの内容
+        /// </summary>
+        public string messageContent{ get; set; }
+        /// <summary>
+        /// 受信日時
+        /// </summary>
+        public long sentTimestamp { get; set; }
+    }
+}
